affa9883dd2e4b5ada3bbeccbcc079528425e786
Minerva <owlmen2546@gmail.com>
2022-07-01 01:44:04 +0700

    before merge

--- mega_clever.py
+++ mega_clever.py
@@ -31,6 +31,37 @@ def block_rotation(block, form):
 
 ##################################################################################################
 
+# check overlap -> check face -> check corner
+def validate_play(board, block, form, row, col):
+
+    # check overlap
+    for i in range(row, row+5):
+        for j in range(col, col+5):
+            if board[i][j] == 1 and block[i-row][j-col] == 1:
+                return False
+    
+    # check face
+    for i in range(row, row+5):
+        for j in range(col, col+5):
+            if block[i-row][j-col] == 1:
+                if board[i-1][j]==1 or board[i+1][j]==1 or board[i][j-1]==1 or board[i][j+1]==1:
+                    return False
+
+    # check corner
+    for i in range(row, row+5):
+        for j in range(col, col+5):
+            if block[i-row][j-col] == 1:
+                if board[i-1][j-1]==1 or board[i-1][j+1]==1 or board[i+1][j-1]==1 or board[i+1][j+1]==1:
+                    return True
+                # temp
+                if (i==0 and j==0) or (i==0 and j==20) or (i==20 and j==0) or (i==20 and j==20):
+                    return True
+
+    return False
+    
+
+##################################################################################################
+
 # initialize board (20*20)
 board = [[0 for _ in range(20)] for _ in range(20)]
 
@@ -191,15 +222,16 @@ while True:
             flag=False
         except:
             print("Only number!!")
-    
-    # place block
-    for i in range(x, x+5):
-        for j in range(y, y+5):
-            board[i][j] = block[z][i-x][j-y]
 
-    # print board
-    print('\n')
-    for i in board:
-        print(i)
+    # place block
+    if validate_play(board, block[z], 0, x, y):
+        for i in range(x, x+5):
+            for j in range(y, y+5):
+                board[i][j] = block[z][i-x][j-y]
+        
+        # print board
+        print('\n')
+        for i in board:
+            print(i)
 
 ##################################################################################################
