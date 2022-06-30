2ca8c5e37bd24bc4318132e10bdbef3596d308ca
Chonapat Chotikulrat <85429191+Chonapatcc@users.noreply.github.com>
2022-07-01 01:42:05 +0700

    while loop input(choose block , to rotate)

--- mega_clever.py
+++ mega_clever.py
@@ -173,10 +173,10 @@ while True:
     # user input
     checker=input("Press ==>'P' to play and ==> 'Q' to quit ==> ")
 
-    if checker=="Q":
+    if checker.lower()=="q":
         print("Turn end.")
         break
-    elif checker=="P":
+    elif checker.lower()=="p":
         print("Start")
     else:
         print("Try again!!!")
@@ -185,14 +185,41 @@ while True:
     flag = True
     while flag:
         try:
+            print("Press path to place a block!! ")
             x=int(input("X = "))
             y=int(input("Y = "))
-            z=int(input("Z = "))
             flag=False
         except:
             print("Only number!!")
+    block_numcheck=True
+    z=0
+    while block_numcheck:
+        print(block[z])
+        blocknum_check=input("Press 'OK' to select and 'NEXT' to choose another block. ")
+        if blocknum_check.lower()=="ok":
+            break
+        elif blocknum_check.lower()!="next":
+            continue
+        z+=1
+        if z>20:
+            z-=20
     
-    # place block
+    rotatedblock_numcheck=True
+    form=0
+    blk=block[z]
+    while rotatedblock_numcheck:
+        print(blk)
+        asktoflip=input("Press 'F' to flip and 'OK' to place a block.")
+        if asktoflip.lower()=="ok":
+            break
+        elif asktoflip.lower()=="f":
+            blk=block_rotation(blk,form)
+            form+=1
+            if form>7:
+                form-=20
+        else:
+            continue
+    # place block    
     for i in range(x, x+5):
         for j in range(y, y+5):
             board[i][j] = block[z][i-x][j-y]
